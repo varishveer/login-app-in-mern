151d81b30b001019c3e408fe072c9ba44487609e
varishveer <variveersingh123@gmail.com>
2023-08-07 00:33:45 +0530

    login app in MERN

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Signup from './signup'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Login from './login'
+import Home from './home'
+
+function App() {
+  
+
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/register' element={<Signup />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/home' element={<Home />}></Route>
+
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
